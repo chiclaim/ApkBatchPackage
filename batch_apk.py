da50c5085137c711e595ed76b0375cabb850f97f
苦木说说 <chiclaim@gmail.com>
2024-03-30 18:10:32 +0800

    解决 V2 前面问题

--- batch_apk.py
+++ batch_apk.py
@@ -1,47 +1,64 @@
-import sys,os,shutil,zipfile,time
-apkVersion="1.0"
-srcFileName="source.apk"
-apksDir="apks"
-destDir=os.path.abspath('.')
-target_file="channel.apk"
-
-file=open("channel.txt")
-
-def writeChannelToApk(filename,channel):
-		z=zipfile.ZipFile(filename,'a',zipfile.ZIP_DEFLATED)
-		empty_channel_file="META-INF/channel_{channe}".format(channe=channel)
-		if not os.path.exists(target_file):
-			open(target_file,'a').close()
-		z.write(target_file,empty_channel_file)
-		z.close()
-		print "writeChannelToApkchannel"+channel+","+filename+"\n"
-
-def cpFile(srcPath,fileName):
-	destPath = destDir + os.path.sep + fileName
-	if os.path.exists(srcPath) and not os.path.exists(destPath):
-		shutil.copy(srcPath,destPath)
-		
-
-		
-if not os.path.exists(srcFileName):
-	print "source file "+srcFileName+" not exists"
-	sys.exit(1)
+import getopt
+import os
+import shutil
+import signal
+import sys
+import time
+import zipfile
 
 
-start = time.clock()
-
-if not os.path.exists(apksDir):
-	os.makedirs(apksDir)
-
-for line in file:
-	channel=line.strip('\n').strip()
-	targetFileName=apksDir+"/hoolay_"+channel+".apk"
-	print "copyfile:"+targetFileName
-	cpFile(srcFileName,targetFileName)
-	writeChannelToApk(targetFileName,channel)
-end = time.clock()
-
-print("The function run time is : %.03f seconds" %(end-start))
+def write_channel_to_apk(apk_file, channel_name):
+    z = zipfile.ZipFile(apk_file, 'a', zipfile.ZIP_DEFLATED)
+    empty_channel_file = f"META-INF/CHANNEL_{channel_name}"
+    temp = "temp.apk"
+    if not os.path.exists(temp):
+        open(temp, 'a').close()
+    z.write(temp, empty_channel_file)
+    z.close()
+    fname, ext = os.path.splitext(apk_file)
+    os.system(f"apksigner sign --ks {your.keystore} --ks-key-alias {your key.alias} --ks-pass pass:{your pass} --key-pass "
+              f"pass:{yourxxxx} --out {fname}_resign{ext} {apk_file}")
+    os.remove(apk_file)
 
 
-#heliao-app-91-5.1.1
+def cp_file(src_file, target_file):
+    if os.path.exists(target_file):
+        os.remove(target_file)
+    shutil.copy(src_file, target_file)
+
+
+def signal_exit_handler(signal_num, frame):
+    sys.exit(signal_num)
+
+
+if __name__ == '__main__':
+    # Ctrl+Z (Mac) 会触发 SIGINT 信号
+    # Ctrl+C (Windows) 会触发 SIGINT 信号
+    signal.signal(signal.SIGINT, signal_exit_handler)
+
+    try:
+        # 第二个参数是短参数，如果没有参数值则不需要写':'(例如 a:b:cd,使用时 -a就必须制定值，-c/-d 则不需要)
+        # 第三个参数是长参数，如果没有参数值则不需要写'='(help，lang=,使用时 --help 不用指定值，--lang就必须制定值，如 --lang=CN)
+        options, args = getopt.getopt(sys.argv[1:], 'h:', ['help'])
+        channel_file = args[0]
+        src_apk_file = args[1]
+        file = open("channel.txt")
+        if not os.path.exists(src_apk_file):
+            print("source file " + src_apk_file + " not exists")
+            sys.exit(1)
+
+        file_name, file_ext = os.path.splitext(src_apk_file)
+        start = time.time()
+
+        for line in file:
+            channel = line.strip('\n').strip()
+            target_file_name = f"{file_name}_{channel}.apk"
+            print(f"复制文件:{target_file_name}")
+            cp_file(src_apk_file, target_file_name)
+            print(f"往{target_file_name}文件写入渠道:{channel}")
+            write_channel_to_apk(target_file_name, channel)
+
+        print("The function run time is : %.03f seconds" % (time.time() - start))
+
+    except getopt.GetoptError as err:
+        print(err)
