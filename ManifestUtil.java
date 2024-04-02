967afadafc53f22967cf9e35b949ae8f51f4c08a
苦木说说 <chiclaim@gmail.com>
2024-04-02 17:44:32 +0800

    Update ManifestUtil.java

--- ManifestUtil.java
+++ ManifestUtil.java
@@ -1,34 +1,28 @@
-package com.hoolay.core.util;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.text.TextUtils;
 
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class ManifestUtil {
+/**
+ * APK 渠道工具类
+ */
+public class ChannelUtil {
 
-    private static final String SP_KEY = "device_channel";
-    public static final String START_FLAG = "META-INF/channel_";
+    private ChannelUtil() {
+    }
+
+    private static final String START_FLAG = "META-INF/channel_";
 
     /**
-     * 获取META-INFO下面的渠道
+     * 获取渠道名
      *
-     * @param context
-     * @return
+     * @return 返回渠道名，没有则返回 null
      */
     public static String getChannel(Context context) {
-        SharedPreferences sp = context.getSharedPreferences("hoolay_sp", Context.MODE_PRIVATE);
-        String channel = sp.getString(SP_KEY, null);
-        if (!TextUtils.isEmpty(channel)) {
-            return channel;
-        }
-        ApplicationInfo appinfo = context.getApplicationInfo();
-        String sourceDir = appinfo.sourceDir;
+        String sourceDir = context.getApplicationInfo().sourceDir;
         ZipFile zipfile = null;
         try {
             zipfile = new ZipFile(sourceDir);
@@ -37,9 +31,7 @@ public class ManifestUtil {
                 ZipEntry entry = ((ZipEntry) entries.nextElement());
                 String entryName = entry.getName();
                 if (entryName.contains(START_FLAG)) {
-                    channel = entryName.replaceAll(START_FLAG, "");
-                    sp.edit().putString(SP_KEY, channel).apply();
-                    return channel;
+                    return entryName.replaceAll(START_FLAG, "");
                 }
             }
         } catch (IOException e) {
@@ -53,7 +45,7 @@ public class ManifestUtil {
                 }
             }
         }
-        return "";
-
+        return null;
     }
+
 }
