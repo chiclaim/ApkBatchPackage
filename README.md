be4197ee08ec733dd8ed20e1c6da272a4e175e50
kumu <kumu@2dfire.com>
2024-04-03 19:22:11 +0800

    批量生成Apk渠道包（支持 V1/V2/V3/V4 签名）

--- README.md
+++ README.md
@@ -1,33 +1,78 @@
-# ApkBatchPackage（支持 V2 签名）
+# 批量生成Apk渠道包（支持 V1/V2/V3 签名）
 
-android apk python 批量打包脚本
+本库的实现原理非常简单，不需要额外的集成依赖。只需要提供签名或未签名的 APK 即可.
 
-
-## 准备工作
+## 使用方法
 
 1. 安装 Python 环境
-2. 编辑你的 channel 文件
+2. 编辑你的 channel 文件:
 
-## 渠道文件
-
-channel.txt
-
-```text
+```
 huawei
 xiaomi
 oppo
 vivo
 ```
 
-
-## 运行脚本
+3. 运行脚本
 
 ```
-python3 batch_apk.py channel.txt [your_apk]
+python3 batch_apk.py channel.txt keystore_config.txt your.apk
 ```
 
-## 程序中获取渠道
+4. 程序中获取渠道
 
 ```
 String channel = ChannelUtil.getChannel(context)
 ```
+
+5. 你可以使用 apksigner 打印生成的渠道包签名信息
+
+```
+Verified using v1 scheme (JAR signing): true
+Verified using v2 scheme (APK Signature Scheme v2): true
+Verified using v3 scheme (APK Signature Scheme v3): true
+Verified using v3.1 scheme (APK Signature Scheme v3.1): false
+Verified using v4 scheme (APK Signature Scheme v4): false
+```
+
+## 市面上主流的多渠道打包工具
+
+市面上主流的多渠道打包工具主要有：腾讯的 WasDolly 和美团的 Walle。主要缺点有：
+
+1. 侵入性大
+   需要依赖各自的 plugin，对最低的 agp 最低版本有要求。例如 WasDolly 最低支持 4.2.0
+
+2. 兼容性问题
+   issue 中有些 闪退问题未解决
+
+## 本库的实现原理
+
+一开始打算使用 WasDolly ，主要是 issue 中存在可能闪退问题，目前还未解决，所以放弃了
+
+后面打算使用 [apktool](https://apktool.org) 反编译，然后添加渠道，然后重新签名，存在两个问题：1. 生成的apk比原先大了 500多kb；
+
+2. 时间也比较长
+apktool 也是第三方维护的，也存在很多 issue 未解决。
+
+所以 apktool 的方案也放弃了
+
+后面想能不能往apk中，写入渠道文件（和V1渠道包方案一样），然后将apk前面文件删除，在重新签名不就好了么。
+apk 本质上就是一个 zip 文件，删除签名文件需要重新压缩，这也可能存在问题，不同的压缩算法生成的文件和原apk可能不一致了，而且安卓的有些资源文件不能参与压缩
+
+所以删除签名文件，在重新签名的方式也放弃
+
+既然要删除签名文件，直接生成一个未签名的APK，然后往 META-INF 写渠道文件不就可以了么，这样当然可以，只不过要先打一个未签名的包，一般我们通过
+Jenkins 打的 release 包都是已经签名可用的包
+
+那能不能直接对已经签名的包，再次签名呢，尝试是可以的。
+
+所以原理很简单提供一个已签名或未签名的 apk，然后往 META-INF 写渠道文件，然后再签名即可。这样既没有侵入性，也没有兼容性问题。理论上是支持后续的签名版本如，V4/V5
+等等（因为没有对代码有任何侵入的改动）
+
+
+
+
+
+
+
--- batch_apk.py
+++ batch_apk.py
@@ -15,9 +15,9 @@ def write_channel_to_apk(apk_file, channel_name):
         open(temp, 'a').close()
     z.write(temp, empty_channel_file)
     z.close()
-    fname, ext = os.path.splitext(apk_file)
-    os.system(f"apksigner sign --ks {your.keystore} --ks-key-alias {your key.alias} --ks-pass pass:{your pass} --key-pass "
-              f"pass:{yourxxxx} --out {fname}_resign{ext} {apk_file}")
+    name, ext = os.path.splitext(apk_file)
+    os.system(f"apksigner sign --ks {ks_path} --ks-key-alias {ks_key_alias} --ks-pass pass:{ks_pass} --key-pass "
+              f"pass:{key_pass} --out {name}_signed{ext} {apk_file}")
     os.remove(apk_file)
 
 
@@ -31,6 +31,31 @@ def signal_exit_handler(signal_num, frame):
     sys.exit(signal_num)
 
 
+ks_path = ""
+ks_key_alias = ""
+ks_pass = ""
+key_pass = ""
+
+
+def parse_ks_config():
+    print(ks_config)
+    with open(ks_config) as f:
+        for config in f:
+            kv = config.split("=")
+            if "ks-path" == kv[0].strip():
+                global ks_path
+                ks_path = kv[1].strip()
+            elif "ks-key-alias" == kv[0].strip():
+                global ks_key_alias
+                ks_key_alias = kv[1].strip()
+            elif "ks-pass" == kv[0].strip():
+                global ks_pass
+                ks_pass = kv[1].strip()
+            elif "key-pass" == kv[0].strip():
+                global key_pass
+                key_pass = kv[1].strip()
+
+
 if __name__ == '__main__':
     # Ctrl+Z (Mac) 会触发 SIGINT 信号
     # Ctrl+C (Windows) 会触发 SIGINT 信号
@@ -41,24 +66,28 @@ if __name__ == '__main__':
         # 第三个参数是长参数，如果没有参数值则不需要写'='(help，lang=,使用时 --help 不用指定值，--lang就必须制定值，如 --lang=CN)
         options, args = getopt.getopt(sys.argv[1:], 'h:', ['help'])
         channel_file = args[0]
-        src_apk_file = args[1]
-        file = open("channel.txt")
+        ks_config = args[1]
+        src_apk_file = args[2]
+
         if not os.path.exists(src_apk_file):
             print("source file " + src_apk_file + " not exists")
             sys.exit(1)
 
+        parse_ks_config()
+
         file_name, file_ext = os.path.splitext(src_apk_file)
         start = time.time()
 
-        for line in file:
-            channel = line.strip('\n').strip()
-            target_file_name = f"{file_name}_{channel}.apk"
-            print(f"复制文件:{target_file_name}")
-            cp_file(src_apk_file, target_file_name)
-            print(f"往{target_file_name}文件写入渠道:{channel}")
-            write_channel_to_apk(target_file_name, channel)
+        with open(channel_file) as file:
+            for line in file:
+                channel = line.strip('\n').strip()
+                target_file_name = f"{file_name}_{channel}.apk"
+                print(f"复制文件:{target_file_name}")
+                cp_file(src_apk_file, target_file_name)
+                print(f"往{target_file_name}文件写入渠道:{channel}")
+                write_channel_to_apk(target_file_name, channel)
 
-        print("The function run time is : %.03f seconds" % (time.time() - start))
+        print("总耗时：%.03f seconds" % (time.time() - start))
 
     except getopt.GetoptError as err:
         print(err)
